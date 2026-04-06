4ee3416106f702419a87d99d3f0913262901d90f
Pradnya Yadav <pradnyayadav21104@gmail.com>
2026-04-06 19:26:40 +0530

    first pyython commit

---
+++ p5.py
@@ -0,0 +1,15 @@
+#Store value 14 and 8 in variable and a and b, swap the values of a and b by using third variable #
+
+a=14
+b=8
+print("Before Swapping")
+print("A=", a)
+print("B=",b)
+
+c=a
+a=b
+b=c
+
+print("After Swapping")
+print("A=",a)
+print("B=",b)
